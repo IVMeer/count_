bd438ea5d13f7fb1884625bf55f9fca271b24dcd
IVMeer <ott1e3r@gmail.com>
2024-12-25 17:41:00 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,29 +8,72 @@ defineProps({
 const count = ref(0)
 const count_miss = ref(0)
 const count_heavy = ref(0)
-const count_model = ref(0)
+const test = ref(0)
+
 
 </script>
 
 <template>
-  <div class="card">
-    <!-- <div >
-      <p>测试:{{ count_model }}</p>
-      <button type="button"      :click="count_model++">+</button>
-      <button :click="count_model--">-</button>
+  <!-- <div class="type1">
+    <button type="button" @click="count++">  +  </button>
+    <button type="button" @click="count++">错 {{ count }}</button>
+    <button type="button" @click="count--">  - </button>
+    <br />
 
-    </div>
-   -->
-    
+    <button type="button" @click="count_miss++">  +  </button>
+    <button type="button" @click="count_miss++">漏 {{ count_miss }}</button>
+    <button type="button" @click="count_miss--">  -  </button>
+    <br />
+    <button type="button" @click="count_heavy++">  +  </button>
+    <button type="button" @click="count_heavy++">多 {{ count_heavy }}</button>
+    <button type="button" @click="count_heavy--">  -  </button>
+  </div> -->
+  <!-- <div class="type2">
+   
     <button type="button" @click="count++">错 {{ count }}</button>
     <button type="button" @click="count_miss++">漏 {{ count_miss }}</button>
     <button type="button" @click="count_heavy++">多 {{ count_heavy }}</button>
+
+  </div>  -->
+
+  <div class="type3">
+      <div class="button-row">
+        <button type="button" @click="count++">+</button>
+        <button type="button" @click="count_miss++">+</button>
+        <button type="button" @click="count_heavy++">+</button>
+      </div>
+      <div class="button-row">
+        <button type="button" @click="count++">错 {{ count }}</button>
+        <button type="button" @click="count_miss++">漏 {{ count_miss}}</button>
+        <button type="button" @click="count_heavy++">多 {{ count_heavy }}</button>
+      </div>
+      <div class="button-row">
+        <button type="button" @click="count--">-</button>
+        <button type="button" @click="count_miss--">-</button>
+        <button type="button" @click="count_heavy--">-</button>
+      </div>
   </div>
-    
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.type3{
+  display: flex;               /* 展示方式flex */
+  flex-direction: column;      /* 垂直排列每一行 */
+  gap: 1px                     /* 每一行之间的间距 */
+}
+.button-row {
+  display: flex;                /* 展示方式flex */
+  justify-content: flex-start;  /* 按钮紧密排列 */
+  margin: 0;                    /* 移除默认的外边距 */
+}
+
+.button-row button{
+  /* flex-grow: 0.5;           */
+  min-width: 32%; 
+  text-align: center;     /* 按钮文本居中 */
+  /* margin: 0;               */
 }
 </style>
--- src/components/fibonacci.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(n):
-    if n < 2:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
-
-
-# n  = 10 
-for i in range(10):
-    print(fibonacci(i), end=" ")
